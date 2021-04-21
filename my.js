665f65b5a7ec771809846785a4df4cb58b13d765
nijo550 <80719041+nijo550@users.noreply.github.com>
2021-04-21 22:59:15 +0200

    Update my.js

--- my.js
+++ my.js
@@ -9,3 +9,21 @@ fetch(url)
 });
 }
 document.getElementById("btn").addEventListener("click", testGS);
+
+function addGS(){
+fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'omit', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({name:"Olivia"}) // body data type must match "Content-Type" header
+  })
+;
+}
+document.getElementById("btn2").addEventListener("click", addGS);
